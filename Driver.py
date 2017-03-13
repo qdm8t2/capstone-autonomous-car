a1f0dbbfc011db735a80039878663a60cfcda3a8
kjsb25 <kjsb25@mail.missouri.edu>
2017-03-12 19:47:00 -0500

    It lives!
    Car Can now be driven using the DriverTester file

--- Driver.py
+++ Driver.py
@@ -1,7 +1,15 @@
 from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor
+from enum import Enum
  
 import time
 import atexit
+
+class Direction(Enum):
+    FORWARD=1
+    BACKWARD=2
+    LEFT=3
+    RIGHT=4
+
 
 class Driver:
 
@@ -14,7 +22,7 @@ class Driver:
         self.lMotors.insert(3,self.mh.getMotor(3))
         self.lMotors.insert(4,self.mh.getMotor(4))
 
-    def driveForward(self,speed,runtime):
+    def drive(self,direction,speed,runtime):
         print("StartMotor")
         for motor in self.lMotors:
             motor.run(Adafruit_MotorHAT.FORWARD)
@@ -31,6 +39,30 @@ class Driver:
             motor.setSpeed(0)
             motor.run(Adafruit_MotorHAT.RELEASE)
         print("Done")
+
+    def turn(self,direction):
+        if direction == Direction.LEFT:
+            mDirection=Adafruit_MotorHAT.FORWARD
+            mDirection=Adafruit_MotorHAT.BACKWARD
+        else:
+            mDirection=Adafruit_MotorHAT.BACKWARD
+            mDirection=Adafruit_MotorHAT.FORWARD
+
+        print("StartMotor")
+        for motor in self.lMotors:
+            motor.run(mDirection)
+            motor.setSpeed(100)
+        for motor in self.rMotors:
+            motor.run(mDirection)
+            motor.setSpeed(100)
+        time.sleep(2)
+        for motor in self.lMotors:
+            motor.setSpeed(0)
+            motor.run(Adafruit_MotorHAT.RELEASE)
+        for motor in self.rMotors:
+            motor.setSpeed(0)
+            motor.run(Adafruit_MotorHAT.RELEASE)
+        
 
 
 
--- DriverTester.py
+++ DriverTester.py
@@ -1,10 +1,26 @@
 from Driver import Driver
+from Driver import Direction
 
 import atexit
 
 driver=Driver();
 
-driver.driveForward(200,2)
+var=raw_input("Command:")
+
+while var!='q':
+	if var=='w':
+		driver.drive(Direction.FORWARD,100,2)
+	if var=='d':
+		driver.turn(Direction.RIGHT)
+	if var=='a':
+		driver.turn(Direction.LEFT)
+	if var=='s':
+		driver.drive(Direction.BACKWARD,100,2)
+
+	var = raw_input("Command:")
+
+
+
 
 
 #THIS NEEDS TO RUN WHEN A DRIVER IS CREATED
