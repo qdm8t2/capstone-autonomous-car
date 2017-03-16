58783066c226dcf28632b9484df6ae401297bc27
tmwbq6 <tmwbq6@mail.missouri.edu>
2017-03-15 23:27:18 -0500

    fixed some cropping adjusted the contrast, resized
    Current image sizes will be less than 1000,1000 maintaining original aspect ratio, will update tomorrow morning with error checking/ testing
    
    
    going to start adjusting the transformations to give us several options to maximize contrast between the floor and our track boundaries

--- ImageTesting.py
+++ ImageTesting.py
@@ -11,35 +11,48 @@ from PIL import ImageOps
 #take in an image, convert to greyscale, convert to an image array, do some transformations, histogram equalization, convert back to an image, save'
 
 def imageprocess (String2, int2):
-    pil_im = Image.open("images/" + String2).convert('L')                               #opens said image converts to greyscale
 
+    pil_im = Image.open("images/" + String2).convert('L')                               #opens said image and converts it to greyscale
+
+
+    #Where do we start cropping from?
 
     halfwidth = pil_im.size[0]/2                                            #right now I crop from the center of the image
-    halfheight = pil_im.size[1]/2                                           #these two lines find the location we are cropping from, right now dead center
+    halfheight = pil_im.size[1]/2                                           #these two lines find the location we are cropping from, as mentioned, dead center
 
 
-    twoThirdwidth = pil_im.size[0]/1.25                                     #not actually 2/3rds anymore but... whatever
-    twoThirdheight = pil_im.size[1]/2.25                                    #These two lines decide how much of the image we get
 
+    #How much of the image do we want to crop out?
+    width = pil_im.size[0]/2.15                                  #how much for the width
+    height = pil_im.size[1]/6                                    #how much for the height
+
+
+    #apply the crop line one: left edge, two: top edge, three: right edge, four: bottom
     pil_im = pil_im.crop(
         (
-            halfwidth - twoThirdwidth,                          #crop
-            halfheight - twoThirdheight,
-            halfwidth + twoThirdwidth,                          #that
-            halfheight + twoThirdheight,
-        )                                                       #shit
+            halfwidth - width,
+            halfheight - height,
+            halfwidth + width,
+            halfheight + halfheight,                    # we want the image as close to the car as possible right? so no cropping done for the bottom edge
+        )
     )
 
-
     pil_im = array(pil_im)                                                  # convert the image to an array
-    pil_im = (100.0 / 255) * pil_im + 100                                   #clamp the values... this might be changed a bit to offer more grey levels ( may provide higher contrast)
+
+
+    # these lines will adjust the contrast in the image
+    pil_im = (100.0 / 255) * pil_im + 90                           #clamp the values...
     pil_im = 255.0 * (pil_im / 255.0) ** 2                                  #quadratic transformation
 
 
     pil_im = Image.fromarray(uint8(pil_im))                                 #in order to do the equalization the easy way, we need an image not an array
-    pil_im = ImageOps.equalize(pil_im)                                      #equalize that shit
+    pil_im = ImageOps.equalize(pil_im)                                      #equalize the image
 
-    String2 = 'images/newpicture0'                                                  #save transformed image as newpicture#
+    size = 1000 , 1000                                                  #define a maximum size for the image. 1000 x 1000 in this case
+
+    pil_im.thumbnail(size, Image.ANTIALIAS)                                 #we use thumbnail to preserve the aspect ratio during the resize, dont want to distort the image in any way
+
+    String2 = 'images/newpicture0'                                                  #save transformed image as newpicture0
     String2 = String2 + str(int2)
     pil_im.save(String2+'.jpg')
     return
@@ -62,12 +75,10 @@ with open("fileInfoses.txt", "r+") as filestream:                               
 
 
 
-#shit i was playing with to figure this stuff out, leaving it here just incase
+#other transformations I looked at to adjust contrast in images. Leaving here as reference for now
 #print( im.shape, im.dtype )
 #im2 = 255 - im #invert image
 #im3 =(100.0/255) * im + 100 #clamp to intervall 1000...200
-#im4 = 255.0 * (im3/255.0)** 1.3 #squared
-#pil_im2 = Image.fromarray(uint8(im4)).save('picture3.jpg')  #.save('picture3.jpg')
-#im5 = ImageOps.equalize(im3 , mask=None)
+
 
 
