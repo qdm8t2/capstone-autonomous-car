f8f547a5c1521fdc004fd9c175e65b2eb55c6f65
tmwbq6 <tmwbq6@mail.missouri.edu>
2017-04-23 12:50:07 -0500

    pydoc
    added pydoc syntax documentation

--- ImageManipulator.py
+++ ImageManipulator.py
@@ -6,7 +6,13 @@ class ImageManipulator:
     """The ImageManipulation Parent Class"""
 
     def _Crop(self, pil_im):
-        # after the most recent camera angle change cropping seems to be not needed
+        """
+        
+        :param pil_im: Input Image (opened elsewhere)  
+        :return: Output Image
+        """
+        # after the most recent camera angle change, cropping seems to be not needed
+
 
         # Where do we start cropping from?
         halfwidth = pil_im.size[0] / 2  # right now I crop from the center of the image
@@ -16,6 +22,7 @@ class ImageManipulator:
         # How much of the image do we want to crop out?
         width = pil_im.size[0] / 2.15  # how much for the width
         height = pil_im.size[1] / 6  # how much for the height
+
 
         # apply the crop line one: left edge, two: top edge, three: right edge, four: bottom
         if ((width + halfwidth) > pil_im.size[0]):
@@ -28,7 +35,7 @@ class ImageManipulator:
                 pil_im.size[1]) + " your proposed size is " + str((height + halfheight)))
             sys.exit()
 
-            # else:
+            # else:                                                                 # actually do the crop
             #    pil_im = pil_im.crop(
             #        (
             #            halfwidth - width,
@@ -37,22 +44,38 @@ class ImageManipulator:
             #            halfheight + halfheight,   #no cropping done on bottom
             #        )
             #    )
-        return pil_im
+        return pil_im                                                   #return the cropped image
 
 
-    def _Resize(self, pil_im):     #Resizes maintaining aspect ratio
+    def _Resize(self, pil_im):     #Resizes maintaining aspect ratio, should be done before any of the _Recolor
+        """
+        
+        :param pil_im: Input Image (opened elsewhere)
+        :return: Output Image
+        """
         size = 100, 100        #defines maximum size
         pil_im.thumbnail(size, Image.ANTIALIAS)  #resizes the image
-        return(pil_im)
+        return(pil_im)                                  #returns the resized image
 
 
 
 class IMRoad(ImageManipulator):
-    """The ImageManipulation road Subclass """
+    """
+    The Image Manipulation 'road' subclass, handles all images of the track 
+    AKA all images not of stop signs 
+    
+    """
     def __init__(self):
+        """
+        Constructor 
+        """
         ImageManipulator.__init__(self)
 
     def _Recolor( pil_im):
+        """
+        Converts to greyscale 
+        :return: returns converted image as an ARRAY  
+        """
 
         res = Image.new(pil_im.mode, pil_im.size)                           # converts to greyscale since the convert('L') wasnt working for some reason
         for i in range(0, pil_im.size[0]):
@@ -73,6 +96,11 @@ class IMRoad(ImageManipulator):
 
         return pil_im
     def process(self, picture):
+        """
+        calls other class functions to process the given image
+        :param picture: input image (already opened) 
+        :return: output image as ARRAY 
+        """
         ImageManipulator._Crop(self, picture)
         ImageManipulator._Resize(self, picture)
         return IMRoad._Recolor(picture)
@@ -80,11 +108,18 @@ class IMRoad(ImageManipulator):
 
 
 class IMStop(ImageManipulator):
-    """The Image Manipulation Stopsign subclass"""
+    """The Image Manipulation 'Stopsign' subclass"""
     def __init__(self):
+        """
+        Constructor
+        """
         ImageManipulator.__init__(self)
 
     def _Recolor(picture):
+        """
+        Converts image to greyscale leaving red pixels
+        :return: converted image as ARRAY
+        """
         red_lower_threshold = 90
         green_blue_diff_threshold = 75
         res = Image.new(picture.mode, picture.size)
@@ -104,6 +139,11 @@ class IMStop(ImageManipulator):
         return res
 
     def process(self, picture):
+        """
+        calls other class functions to process the given image
+        :param picture: input image (already opened) 
+        :return: output image as ARRAY 
+        """
         ImageManipulator._Crop(self, picture)
         ImageManipulator._Resize(self, picture)
         return IMStop._Recolor(picture)
